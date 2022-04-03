666ce8675ea209d984ea68d1830b1c24aef21878
Nitesh Saini <Nitesh.saini2402@outlook.com>
2022-04-03 13:12:14 +0530

    email form github

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,15 +1,44 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "niteshsaini2402@gmail.com"; // this is your Email address
-    $from_email = $_POST['email']; // this is the sender's Email address
-    $from_name = $_POST['name'];
-    $subject = $_POST['subject']
-    $message = $from_name . " wrote the following:" . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from_email;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = $email; 
+		
+		// WARNING: Be sure to change this. This is the address that the email will be sent to
+		$to = 'niteshsaini2402@gmail.com'; 
+		
+		$subject = "Message from ".$name." ";
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
 ?>
